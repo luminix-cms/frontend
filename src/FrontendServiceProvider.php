3feb9f0f51fc08ada7e4c8fbef50809a4a5f28df
Bruno Polo <brunopolo@poli.ufrj.br>
2024-03-03 13:52:18 -0300

    first commit

---
+++ src/FrontendServiceProvider.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Luminix\Frontend;
+
+use Illuminate\Support\ServiceProvider;
+
+class FrontendServiceProvider extends ServiceProvider
+{
+    public function boot()
+    {
+        
+    }
+
+    public function register()
+    {
+        
+    }
+}
