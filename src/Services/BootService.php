bacadb241f129d3bcf4b99ee64ab8cf4767f01ee
Bruno Polo <brunopolo@poli.ufrj.br>
2025-11-28 13:26:28 -0300

    fixes application url exposal in BootService when using custom port

--- src/Services/BootService.php
+++ src/Services/BootService.php
@@ -16,7 +16,7 @@ class BootService {
                 'name' => config('app.name', 'Laravel'),
                 'env' => config('app.env', 'production'),
                 'debug' => config('app.debug', false),
-                'url' => config('app.url', 'http://localhost'),
+                'url' => url('/'),
                 'locale' => config('app.locale', 'en'),
                 'fallback_locale' => config('app.fallback_locale', 'en'),
             ],
